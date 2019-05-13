12b65dcce01096110eb6c99e817559cdb1401ea9
朝野布告 <chaoyebugao@msn.cn>
2019-05-13 18:39:58 +0800

    登录和授权

---
+++ AcctAuthDemo.Web/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcctAuthDemo.Web.Controllers
+{
+    /// <summary>
+    /// 用户
+    /// </summary>
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string name, string password)
+        {
+            return View();
+        }
+    }
+}
